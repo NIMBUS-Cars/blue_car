1164523ba3b2b1944252ad7d28b5a2bea850a68d
JonahBurke <jonahburke777@gmail.com>
2022-03-21 12:46:11 -0500

    Safety node no longer uses "brake" and "brake_bool" topics because they don't exist on the robot

--- scripts/safety.py
+++ scripts/safety.py
@@ -49,23 +49,17 @@ def calculate_ttc(r, v, theta):
     denominator = -1*v*math.cos(theta)
     if denominator <= 0: # this is basically doing the max(x,0) calculation, but helps us avoid math issues
         return float('inf')
-    else:
-        return r / denominator
+    return r / denominator
 
 # This safety class is based on https://github.com/f1tenth/f1tenth_labs/blob/main/lab2/code/scripts/safety_node.py
 class Safety(object):
     def __init__(self):
-        self.speed = 0
+        self.speed = 1.0
         self.ttc_threshold = 0.5
         rospy.Subscriber('/scan', LaserScan, self.scan_callback)
-        rospy.Subscriber('/vesc/odom', Odometry, self.odom_callback)
-        self.brake = rospy.Publisher('brake', AckermannDriveStamped, queue_size=10)
-        self.brake_msg = AckermannDriveStamped()
-        self.brake_bool = rospy.Publisher('brake_bool', Bool, queue_size=10)
-        self.brake_bool_msg = Bool()
-
-    def odom_callback(self, odom_msg):
-        self.speed = odom_msg.twist.twist.linear.x
+        self.drive = rospy.Publisher(rospy.get_param('/nav_drive_topic'), AckermannDriveStamped, queue_size=10)
+        #self.drive = rospy.Publisher("/vesc/high_level/ackermann_cmd_mux/input/nav_0", AckermannDriveStamped, queue_size=10)
+        self.drive_msg = AckermannDriveStamped()
 
     def scan_callback(self, scan_msg):
         # Calculate the TTC around the vehicle (assuming ranges[0] is straight behind)
@@ -74,25 +68,15 @@ class Safety(object):
             ttc = calculate_ttc(scan_msg.ranges[i], self.speed, i*scan_msg.angle_increment)
             min_ttc = min(min_ttc, ttc)
 
-        # TODO: publish brake message and publish controller bool
-        self.brake_msg.drive.speed = 0.0
-        self.brake.publish(self.brake_msg)
-        self.brake_bool_msg.data = (min_ttc <= self.ttc_threshold)
-        self.brake_bool.publish(self.brake_bool_msg)
+        if min_ttc <= self.ttc_threshold:
+            self.speed = 0
+        self.drive_msg.drive.speed = self.speed
+        self.drive.publish(self.drive_msg)
 
 
 def main():
     rospy.init_node('safety')
-    #sn = Safety()
-    #drive_topic = rospy.get_param('/nav_drive_topic')
-    drive = rospy.Publisher("/vesc/high_level/ackermann_cmd_mux/input/nav_0", AckermannDriveStamped, queue_size=10)
-    drive_msg = AckermannDriveStamped()
-    drive_msg.drive.speed = 1.0
-    rospy.loginfo('Jonah Says Hello!!!!!')
-    rate = rospy.Rate(10)
-    while not rospy.is_shutdown():
-        drive.publish(drive_msg)
-        rate.sleep()
+    sn = Safety()
     rospy.spin()
 
 if __name__ == '__main__':
