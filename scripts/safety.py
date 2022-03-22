25312ce34878790d3c74d2052c86658ec41bfe49
JonahBurke <jonahburke777@gmail.com>
2022-03-22 14:56:38 -0500

    Update safety.py

--- scripts/safety.py
+++ scripts/safety.py
@@ -60,7 +60,7 @@ class Safety(object):
         self.drive = rospy.Publisher(rospy.get_param('/nav_drive_topic'), AckermannDriveStamped, queue_size=10)
         #self.drive = rospy.Publisher("/vesc/high_level/ackermann_cmd_mux/input/nav_0", AckermannDriveStamped, queue_size=10)
         self.drive_msg = AckermannDriveStamped()
-        f = open("~/test_document.txt", "w")
+        f = open("test_document.txt", "w")
         f.write("This is a test!")
         f.close()
         rospy.loginfo("!!!!!!! It Ran !!!!!!!!")
