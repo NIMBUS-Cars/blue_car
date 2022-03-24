e58168f9b19a89a5b573557ec3dbc4f30ca4744b
JonahBurke <jonahburke777@gmail.com>
2022-03-24 16:12:03 -0500

    5 meter test

--- scripts/safety.py
+++ scripts/safety.py
@@ -54,9 +54,9 @@ def calculate_ttc(r, v, theta):
 # This safety class is based on https://github.com/f1tenth/f1tenth_labs/blob/main/lab2/code/scripts/safety_node.py
 class Safety(object):
     def __init__(self):
-        self.speed = 1.0
+        self.speed = 1.25
         self.ttc_threshold = 1.0
-        self.output_file = "ttc_data_4m.txt"
+        self.output_file = "ttc_data_5m.txt"
         rospy.Subscriber('/scan', LaserScan, self.scan_callback)
         self.drive = rospy.Publisher(rospy.get_param('/nav_drive_topic'), AckermannDriveStamped, queue_size=10)
         self.drive_msg = AckermannDriveStamped()
