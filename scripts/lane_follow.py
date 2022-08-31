34cd383437c351c86c13b55414d8509454cb00b8
Yuntao <lyt19971227@gmail.com>
2022-08-30 20:03:33 -0500

    testing steering

--- scripts/lane_follow.py
+++ scripts/lane_follow.py
@@ -13,6 +13,7 @@ import numpy as np
 import cv_bridge
 import cv2
 import rospy
+import math
 
 
 class Follower:
@@ -32,11 +33,11 @@ class Follower:
     def image_callback(self, msg):
         try:
             image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
-            h, w, d = image.shape
+            h1, w1, d1 = image.shape
             # 480x640
-            # rospy.loginfo("h %s", h)
-            # rospy.loginfo("w %s", w)
-            image_crop = image[h/2:h, w/4:3*w/4]
+            # rospy.loginfo("h %s", h1)
+            # rospy.loginfo("w %s", w1)
+            image_crop = image[h1/2:h1, w1/4:3*w1/4]
             hsv = cv2.cvtColor(image_crop, cv2.COLOR_BGR2HSV)
 
             # change below lines to map the color you wanted robot to follow
@@ -48,10 +49,10 @@ class Follower:
             # rospy.loginfo("M %s", M)
 
             if M['m00'] > 0:
-                h, w, d = image_crop.shape
+                h2, w2, d2 = image_crop.shape
                 # # 180x400
-                rospy.loginfo("h %s", h)
-                rospy.loginfo("w %s", w)
+                rospy.loginfo("h %s", h2)
+                rospy.loginfo("w %s", w2)
                 cx = int(M['m10']/M['m00'])
                 cy = int(M['m01']/M['m00'])
                 rospy.loginfo("cx %s", cx)
@@ -61,9 +62,11 @@ class Follower:
                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (125, 125, 125), 2)
                 # CONTROL starts
                 # Steering Calculation
-                # if cx > w
-                # steering_angle = 
-                self.drive_msg.drive.steering_angle = 0.8
+                if cx < w2/2:
+                    steering_angle = -math.atan2(h2-cy, cx-(w2/2))
+                else:
+                    steering_angle = math.atan2(h2-cy, cx-(w2/2))
+                self.drive_msg.drive.steering_angle = steering_angle
                 self.drive.publish(self.drive_msg)
                 # CONTROL ends
             cv2.imshow("original image", image)
