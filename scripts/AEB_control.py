61a073c710a34e7f4b9e297acfb4883e1a0744b3
Yuntao <lyt19971227@gmail.com>
2022-09-28 16:52:22 -0500

    AEB testing

--- scripts/AEB_control.py
+++ scripts/AEB_control.py
@@ -22,34 +22,38 @@ def TTC_calc(r_i, v_x, theta):
         TTC_i = r_i / (-r_i_dot)
     return TTC_i
 
+def calculate_ttc(r, v, theta):
+    denominator = -1*v*math.cos(theta)
+    if denominator <= 0: # this is basically doing the max(x,0) calculation, but helps us avoid math issues
+        return float('inf')
+    return r / denominator
 
 class AEB:
     def __init__(self):
-        self.lidar_sub = rospy.Subscriber('/scan',
-                                          LaserScan, self.lidar_callback)
-        self.drive = rospy.Publisher(rospy.get_param(
-            '/nav_drive_topic'), AckermannDriveStamped, queue_size=10)
-        self.drive_msg = AckermannDriveStamped()
+        self.speed = 1.5
         self.ttc_threshold = 1.0
-        self.speed = 1.0
+        self.output_file = "5m.txt"
+        rospy.Subscriber('/scan', LaserScan, self.scan_callback)
+        self.drive = rospy.Publisher(rospy.get_param('/nav_drive_topic'), AckermannDriveStamped, queue_size=10)
+        self.drive_msg = AckermannDriveStamped()
+        # self.ttc_data_file = open(self.output_file, "w") # This file is located in ~/.ros
+        # self.ttc_data_file.write("ttc_threshold, min_ttc, speed\n")
+        # self.ttc_data_file.close()
 
-    def lidar_callback(self, lidar_msg):
-        # rospy.loginfo("lidar_callback")
-        # rospy.loginfo("lidar_msg %s", lidar_msg)
+    def scan_callback(self, scan_msg):
+        # Calculate the TTC around the vehicle (assuming ranges[0] is straight behind)
         min_ttc = float('inf')
-        # self.speed = 0.5
+        for i in range(0,len(scan_msg.ranges),2): # increment by 2 to reduce calculation time
+            ttc = calculate_ttc(scan_msg.ranges[i], self.speed, i*scan_msg.angle_increment)
+            min_ttc = min(min_ttc, ttc)
 
-        # increment by 2 to reduce calculation time
-        for i in range(0, len(lidar_msg.ranges), 2):
-            rospy.loginfo("lidar_msg %s", lidar_msg.ranges[i])
-            # ttc = TTC_calc(
-            #     lidar_msg.ranges[i], self.speed, i*lidar_msg.angle_increment)
-            # min_ttc = min(min_ttc, ttc)
-            # rospy.loginfo("min_ttc %s", min_ttc)
+        if min_ttc <= self.ttc_threshold:
+            self.speed = 0
 
-        # if min_ttc <= self.ttc_threshold:
-        #     rospy.loginfo("Apply brake!")
-        #     self.speed = 0
+        # if min_ttc != float('inf'):
+        #     self.ttc_data_file = open(self.output_file, "a")
+        #     self.ttc_data_file.write(str(self.ttc_threshold) + ", " + str(min_ttc) + ", " + str(self.speed) + "\n")
+        #     self.ttc_data_file.close()
 
         self.drive_msg.drive.speed = self.speed
         self.drive.publish(self.drive_msg)
