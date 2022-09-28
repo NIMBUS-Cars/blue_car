f6b8bbd711e07d050620ef9933053c874c2b231a
Yuntao <lyt19971227@gmail.com>
2022-09-28 17:50:11 -0500

    AEB testing

--- scripts/safety_node.py
+++ scripts/safety_node.py
@@ -8,7 +8,6 @@ from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
 from std_msgs.msg import Bool
 from ackermann_msgs.msg import AckermannDriveStamped
-from ackermann_msgs.msg import AckermannDrive
 
 
 class Safety(object):
@@ -17,31 +16,19 @@ class Safety(object):
     """
 
     def __init__(self):
-        """
-        One publisher should publish to the /brake topic with a AckermannDriveStamped brake message.
-
-        One publisher should publish to the /brake_bool topic with a Bool message.
-
-        You should also subscribe to the /scan topic to get the LaserScan messages and
-        the /odom topic to get the current speed of the vehicle.
-
-        The subscribers should use the provided odom_callback and scan_callback as callback methods
-
-        NOTE that the x component of the linear velocity in odom is the speed
-        """
-        self.speed = 0.3
+        self.speed = 0.5
+        self.steering = 0
         # TODO: create ROS subscribers and publishers.
-        self.laser_scan_sub = rospy.Subscriber("/scan", LaserScan, self.scan_callback)
+        self.laser_scan_sub = rospy.Subscriber(
+            "/scan", LaserScan, self.scan_callback)
         # self.odom_sub = rospy.Subscriber("/vesc/odom", Odometry, self.odom_callback)
         self.drive_topic = rospy.get_param("/nav_drive_topic")
-        self.drive_pub = rospy.Publisher(self.drive_topic, AckermannDriveStamped, queue_size=10)
-        self.brake_bool_pub = rospy.Publisher("/brake_bool", Bool, queue_size=10)
-        self.ttc_threshhold = 0.5
+        self.drive_pub = rospy.Publisher(
+            self.drive_topic, AckermannDriveStamped, queue_size=10)
+        self.brake_bool_pub = rospy.Publisher(
+            "/brake_bool", Bool, queue_size=10)
+        self.ttc_threshhold = 0.8
         self.drive_msg = AckermannDriveStamped()
-
-    # def odom_callback(self, odom_msg):
-    #     # TODO: update current speed
-    #     self.speed = odom_msg.twist.twist.linear.x
 
     def scan_callback(self, scan_msg):
         # TODO: calculate TTC
@@ -52,48 +39,49 @@ class Safety(object):
             # print("scan_msg: ", scan_msg)
             # rospy.loginfo("speed: ", self.speed)
 
-            # # steeringAngle = 0.00
-            # # if(self.speed <= 0.2):
-            # #     steeringAngle = steeringAngle / 0.75 * 0.9
-            # # rospy.loginfo("steering angle: ", self.drive_msg.steering_angle)
+            # steeringAngle = 0.00
+            # if(self.speed <= 0.2):
+            #     steeringAngle = steeringAngle / 0.75 * 0.9
+            # rospy.loginfo("steering angle: ", self.drive_msg.steering_angle)
 
-            # #fixed_angle_min = scan_msg.angle_min + 1.57
-            # #fixed_angle_max = scan_msg.angle_max - 1.57
-            # fixed_angle_min = scan_msg.angle_min + 2
-            # fixed_angle_max = scan_msg.angle_max - 2
+            #fixed_angle_min = scan_msg.angle_min + 1.57
+            #fixed_angle_max = scan_msg.angle_max - 1.57
+            fixed_angle_min = scan_msg.angle_min + 2
+            fixed_angle_max = scan_msg.angle_max - 2
             # rospy.loginfo("fixed angle min: ",  fixed_angle_min)
             # rospy.loginfo("fixed angle max: ",  fixed_angle_max)
 
-            # self.angles_array = np.arange(fixed_angle_min, fixed_angle_max, scan_msg.angle_increment)
-            # self.ranges_array = np.array(scan_msg.ranges)
+            self.angles_array = np.arange(
+                fixed_angle_min, fixed_angle_max, scan_msg.angle_increment)
+            self.ranges_array = np.array(scan_msg.ranges)
             # rospy.loginfo("ranges_array: ",  self.ranges_array)
 
-            # # fix denominator
-            # # option 1 --------
-            # # self.range_rates = np.max(
-            # #    self.speed * np.cos(self.angles_array), 0) + 0.000000001
-            # # self.ttcs = (self.ranges_array/self.range_rates)
-            # # --------------
-            # # option 2 ----------
-            # denominator = np.max(self.speed * np.cos(self.angles_array), 0)
-            # if (denominator == 0):
-            #     self.ttcs = np.inf
-            # else:
-            #     self.range_rates = denominator
-            #     self.ttcs = (self.ranges_array/self.range_rates)
-            # # ------------
-            # # find the minimum ttc value
-            # self.min_ttc = np.min(self.ttcs)
-            # rospy.loginfo("min ttc: ", self.min_ttc)
+            # fix denominator
+            # option 1 --------
+            # self.range_rates = np.max(
+            #    self.speed * np.cos(self.angles_array), 0) + 0.000000001
+            # self.ttcs = (self.ranges_array/self.range_rates)
+            # --------------
+            # option 2 ----------
+            denominator = np.max(self.speed * np.cos(self.angles_array), 0)
+            if (denominator == 0):
+                self.ttcs = np.inf
+            else:
+                self.range_rates = denominator
+                self.ttcs = (self.ranges_array/self.range_rates)
+            # ------------
+            # find the minimum ttc value
+            self.min_ttc = np.min(self.ttcs)
+            rospy.loginfo("min ttc: ", self.min_ttc)
 
-            # # TODO: publish brake message and publish controller bool
-            # if self.min_ttc < self.ttc_threshhold:
-            #     rospy.loginfo("Min TTC below Threshhold, Apply brake here")
-            #     self.brake_bool_pub.publish(True)
-            #     self.speed = 0.0
+            # TODO: publish brake message and publish controller bool
+            if self.min_ttc < self.ttc_threshhold:
+                rospy.loginfo("Min TTC below Threshhold, Apply brake here")
+                # self.brake_bool_pub.publish(True)
+                self.speed = 0.0
 
             # else:
-            #     self.brake_bool_pub.publish(False)
+                # self.brake_bool_pub.publish(False)
 
             # self.drive_msg.steering_angle = steeringAngle*-0.75
             self.drive_msg.drive.speed = self.speed
