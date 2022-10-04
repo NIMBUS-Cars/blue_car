d3b138b77bde4bcc2e769e7d699585d0d92a876a
929937690 <44484569+929937690@users.noreply.github.com>
2022-10-04 15:53:21 -0500

    Update AEB_control.py

--- scripts/AEB_control.py
+++ scripts/AEB_control.py
@@ -33,17 +33,17 @@ class AEB:
         self.drive_msg = AckermannDriveStamped()
 
     def scan_callback(self, scan_msg):
-#         # Calculate the TTC around the vehicle, angle 0 is front
-#         min_ttc = float('inf')
-#         # increment by 2 to reduce calculation time
+        # Calculate the TTC around the vehicle, angle 0 is front
+        min_ttc = float('inf')
+        # increment by 2 to reduce calculation time
 #         forward_angle = 0
 #         min_angle = scan_msg.angle_min
-#         for i in range(0, len(scan_msg.ranges), 2):
-#             # theta = abs(forward_angle - (min_angle + i * scan_msg.angle_increment))
-#             # rospy.loginfo("theta: %s", theta)
-#             ttc = TTC_calc(
-#                 scan_msg.ranges[i], self.speed, i * scan_msg.angle_increment)
-#             min_ttc = min(min_ttc, ttc)
+        for i in range(0, len(scan_msg.ranges), 2):
+            # theta = abs(forward_angle - (min_angle + i * scan_msg.angle_increment))
+            # rospy.loginfo("theta: %s", theta)
+            ttc = TTC_calc(
+                scan_msg.ranges[i], self.speed, i * scan_msg.angle_increment)
+            min_ttc = min(min_ttc, ttc)
 
 #         if min_ttc <= self.ttc_threshold:
 #             rospy.loginfo("Min TTC below Threshhold, Apply brake here")
