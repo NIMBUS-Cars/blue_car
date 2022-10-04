fb4a917ee0d2da783b1e3d1c86e021f307a8c44a
929937690 <44484569+929937690@users.noreply.github.com>
2022-10-04 15:47:31 -0500

    Update AEB_control.py

--- scripts/AEB_control.py
+++ scripts/AEB_control.py
@@ -25,7 +25,7 @@ def TTC_calc(r_i, v_x, theta):
 
 class AEB:
     def __init__(self):
-        self.speed = 2.0
+        self.speed = 1.0
         self.ttc_threshold = 1.0
         rospy.Subscriber('/scan', LaserScan, self.scan_callback)
         self.drive = rospy.Publisher(rospy.get_param(
